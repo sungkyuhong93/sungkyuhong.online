7ee2841114378fea606e8937e4f88660348fdf9e
Sung Kyu Hong <sungkyuhong93@gmail.com>
2024-04-23 18:07:19 -0400

    add db logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ server.on('request', app);
 
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
+process.on('SIGINT', () => {
+  console.log('sigint');
+  wss.clients.forEach(function each(client) {
+    client.close();
+  })
+  server.close(() => {
+    shutdownDB();
+  });
+})
+
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
 
@@ -26,6 +36,10 @@ wss.on('connection', function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send('welcome!');
   }
+
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
 
   ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${wss.clients.size}`);
@@ -49,3 +63,28 @@ wss.broadcast = function broadcast(data) {
   });
 };
 /** End Websocket **/
+
+/** begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `)
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log('Shutting down db');
+  db.close();
+}
